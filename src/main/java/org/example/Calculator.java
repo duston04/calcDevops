250f03b0c5690238cfccf3f13089b1f6b8a609b9
duston04 <darshankatkar44@gmail.com>
2023-03-15 18:58:17 +0530

    Monitoring added

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,84 +1,106 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.example.services.Factorial;
 import org.example.services.Logrithm;
 import org.example.services.Power;
 import org.example.services.SqRoot;
 
+
 import java.util.Scanner;
 
 public class Calculator
 {
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
     public static void main( String[] args )
     {
+        Calculator calculator = new Calculator();
         double a;
         double b;
         int choice;
         Scanner sc = new Scanner(System.in);
 
-        while(true){
+        while(true) {
 
             System.out.println("1. Square root function - √x \n2. Factorial function - x! \n3. Natural logarithm (base е) - ln(x) \n4. Power function - x^b \n5. Exit\n");
             System.out.print("Enter your choice : ");
             try {
                 choice = sc.nextInt();
 
-                if(choice == 5){
+                if (choice == 5) {
                     System.out.println("\nBye and thank you for using this app");
                     System.out.println("\nx-x-x-x-x-x-x-x-x-x-x-x-x-x");
                     break;
-                }
-
-                else if(choice == 1){
+                } else if (choice == 1) {
                     System.out.print("Enter the number you want the square root of : ");
                     a = sc.nextDouble();
                     SqRoot sqrt = new SqRoot();
                     double result = sqrt.root(a);
-                    System.out.println("The square root of "+a+" is "+result);
+                    System.out.println("The square root of " + a + " is " + result);
                     System.out.println("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n");
-                }
-
-                else if(choice == 2){
+                } else if (choice == 2) {
                     System.out.print("Enter the number you want the Factorial of : ");
                     a = sc.nextDouble();
                     Factorial fact = new Factorial();
                     double result = fact.factorial(a);
-                    System.out.println("The Factorial of "+a+" is "+result);
+                    System.out.println("The Factorial of " + a + " is " + result);
                     System.out.println("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n");
-                }
-
-                else if(choice == 3){
+                } else if (choice == 3) {
                     System.out.print("Enter the number you want the log of : ");
                     a = sc.nextDouble();
                     Logrithm logarithm = new Logrithm();
                     double result = logarithm.log(a);
-                    System.out.println("The natural log of "+a+" is "+result);
+                    System.out.println("The natural log of " + a + " is " + result);
                     System.out.println("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n");
-                }
-
-                else if(choice == 4){
+                } else if (choice == 4) {
                     System.out.print("Enter the base : ");
                     a = sc.nextDouble();
                     System.out.print("Enter the exponent : ");
                     b = sc.nextDouble();
                     Power power = new Power();
-                    double result = power.power(a,b);
-                    System.out.println("The result of ("+a+" ^ "+b+") is "+result);
+                    double result = power.power(a, b);
+                    System.out.println("The result of (" + a + " ^ " + b + ") is " + result);
                     System.out.println("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n");
-                }
-
-                else if(choice <= 0 || choice > 5){
+                } else if (choice <= 0 || choice > 5) {
                     System.out.println("The choice is invalid....Try again");
                     System.out.println("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n");
                 }
 
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println(e);
                 System.out.println("\nApp crashed... Bye and see you again");
                 System.out.println("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n");
                 break;
             }
         }
+    }
 
+    public double power(double a, double b) {
+        logger.info("[Power of - " + a + " raise to " + b);
+        double result = power(a,b);
+        logger.info("[RESULT - POWER Function] - " + result);
+        return result;
+    }
+
+    public double sqareroot(double a) {
+        logger.info("[SQUARE ROOT] - " + a);
+        double result = Math.sqrt(a);
+        logger.info("[RESULT - SQUARE ROOT] - " + result);
+        return result;
+    }
+
+    public double factorial(double a) {
+        logger.info("[FACTORIAL] - " + a);
+        double result = factorial(a);
+        logger.info("[RESULT - FACTORIAL] - " + result);
+        return result;
+    }
+
+    public double sub(double number1, double number2) {
+        logger.info("[SUBTRACTION - " + number2 + " FROM " + number1);
+        double result = number1 - number2;
+        logger.info("[RESULT - SUBTRACTION] - " + result);
+        return result;
     }
 }
