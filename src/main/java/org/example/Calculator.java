f8bc692f38d31a8113a7d202ebb7a5a7ac3bbe02
duston04 <darshankatkar44@gmail.com>
2023-03-15 22:58:45 +0530

    Monitoring added

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -13,6 +13,9 @@ import java.util.Scanner;
 public class Calculator
 {
     private static final Logger logger = LogManager.getLogger(Calculator.class);
+
+    public Calculator() {
+    }
     public static void main( String[] args )
     {
         Calculator calculator = new Calculator();
@@ -34,22 +37,16 @@ public class Calculator
                 } else if (choice == 1) {
                     System.out.print("Enter the number you want the square root of : ");
                     a = sc.nextDouble();
-                    SqRoot sqrt = new SqRoot();
-                    double result = sqrt.root(a);
                     System.out.println("The square root of " + a + " is " + calculator.sqareroot(a));
                     System.out.println("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n");
                 } else if (choice == 2) {
                     System.out.print("Enter the number you want the Factorial of : ");
                     a = sc.nextDouble();
-                    Factorial fact = new Factorial();
-                    double result = fact.factorial(a);
                     System.out.println("The Factorial of " + a + " is " + calculator.factorial(a));
                     System.out.println("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n");
                 } else if (choice == 3) {
                     System.out.print("Enter the number you want the log of : ");
                     a = sc.nextDouble();
-                    Logrithm logarithm = new Logrithm();
-                    double result = logarithm.log(a);
                     System.out.println("The natural log of " + a + " is " + calculator.naturallog(a));
                     System.out.println("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n");
                 } else if (choice == 4) {
@@ -57,8 +54,6 @@ public class Calculator
                     a = sc.nextDouble();
                     System.out.print("Enter the exponent : ");
                     b = sc.nextDouble();
-                    Power power = new Power();
-                    double result = power.power(a, b);
                     System.out.println("The result of (" + a + " ^ " + b + ") is " + calculator.power(a,b));
                     System.out.println("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n");
                 } else if (choice <= 0 || choice > 5) {
@@ -77,28 +72,32 @@ public class Calculator
 
     public double power(double a, double b) {
         logger.info("[Power of - " + a + " raise to " + b);
-        double result = power(a,b);
+        Power pow = new Power();
+        double result = pow.power(a, b);
         logger.info("[RESULT - POWER Function] - " + result);
         return result;
     }
 
     public double sqareroot(double a) {
         logger.info("[SQUARE ROOT] - " + a);
-        double result = Math.sqrt(a);
+        SqRoot sqrt = new SqRoot();
+        double result = sqrt.root(a);
         logger.info("[RESULT - SQUARE ROOT] - " + result);
         return result;
     }
 
     public double factorial(double a) {
         logger.info("[FACTORIAL] - " + a);
-        double result = factorial(a);
+        Factorial fact = new Factorial();
+        double result = fact.factorial(a);
         logger.info("[RESULT - FACTORIAL] - " + result);
         return result;
     }
 
     public double naturallog(double a) {
         logger.info("[NATURAL LOG] - " + a);
-        double result = naturallog(a);
+        Logrithm logarithm = new Logrithm();
+        double result = logarithm.log(a);
         logger.info("[RESULT - NATURAL LOG] - " + result);
         return result;
     }
